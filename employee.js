cc99e76449b322335e080d344b8367d2788623a2
Reed Keyes <rbkeyes@gmail.com>
2023-01-06 13:10:51 -0800

    added method sections getName, getId, getEmail, getRole

--- employee.js
+++ employee.js
@@ -1,7 +1,17 @@
+const { getSystemErrorName } = require("util");
+
 class Employee {
     constructor(name, id, email) {
         this.name = name;
         this.id = id;
         this.email = email;
+
+        getName()
+
+        getId()
+
+        getEmail()
+
+        getRole()
     }
 }
