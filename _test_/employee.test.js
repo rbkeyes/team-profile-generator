f6e4f9003d26c7c30ecac9f333893ccce4d80420
Reed Keyes <rbkeyes@gmail.com>
2023-01-08 14:46:07 -0800

    code added to employee.test.js for getID, getEmail, and getRole. All tests pass when run with npm test

--- _test_/employee.test.js
+++ _test_/employee.test.js
@@ -1,20 +1,53 @@
-// const { exportAllDeclaration } = require('@babel/types');
-// const { it } = require('node:test');
+// class contructor
 const Employee = require('../lib/employee');
 
+// const to be used for test
+const employeeObj = { name: 'Bob', id: "1", email: "email@email.com" };
+const employeeTest = new Employee("Bob", "1", "email@email.com");
+
+
 describe('Employee class', () => {
-    const employeeObj = {name: 'Bob', id: "1", email: "email@email.com"};
-    it('Should return an employee object', () => {
-        const employeeTest = new Employee("Bob", "1", "email@email.com");
+    it('Should return an employee object containing name, id, and email', () => {
         expect(employeeTest).toEqual(employeeObj);
     });
-    
+
     describe('getName', () => {
         it('Should return this.name', () => {
             const employeeName = employeeObj.name;
-            const nameTest = Employee.getName('Bob')
-
+            console.log(employeeName);
+            const nameTest = employeeTest.getName();
+            console.log(nameTest);
             expect(nameTest).toEqual(employeeName);
         })
-    })
-})
+    });
+
+    describe('getID', () => {
+        it('Should return this.id', () => {
+            const employeeId = employeeObj.id;
+            console.log(employeeId);
+            const idTest = employeeTest.getId();
+            console.log(idTest);
+            expect(idTest).toEqual(employeeId);
+        })
+    });
+
+    describe('getEmail', () => {
+        it('Should return this.email', () => {
+            const employeeEmail = employeeObj.email;
+            console.log(employeeEmail);
+            const emailTest = employeeTest.getEmail();
+            console.log(emailTest);
+            expect(emailTest).toEqual(employeeEmail);
+        })
+    });
+
+    describe('getRole', () => {
+        it('Should return "Employee"', () => {
+            const employeeRole = "Employee";
+            console.log(employeeRole);
+            const roleTest = employeeTest.getRole();
+            console.log(roleTest);
+            expect(roleTest).toEqual(employeeRole);
+        })
+    });
+});
