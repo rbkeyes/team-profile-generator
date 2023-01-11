ca7fa402074a9e96cfc96b13999c9a50871f43b5
Reed Keyes <rbkeyes@gmail.com>
2023-01-11 11:37:41 -0800

    added code for add employee, prompts for all employee data

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 // inquirer
 const inquirer = require('inquirer');
+
 // fs
 const fs = require('fs');
+
+// prompts file
+const prompts = require('./prompts')
 
 // constructors
 const Employee = require('./lib/employee');
@@ -9,25 +13,71 @@ const Manager = require('./lib/manager');
 const Engineer = require('./lib/engineer');
 const Intern = require('./lib/intern');
 
-// other variables
-const employees = [];
+// will hold array of employees
+const employees = '';
 
+// employee objects to be written to file
+const managerPrompts = [prompts.name, prompts.id, prompts.email, prompts.officeNum];
+console.log(managerPrompts);
+const engineerObj = '';
+const internObj = '';
 
-
-console.log(addlInfo.length);
-
-function addManager() {
-    // console.log(employees);
-    // if (employees === []) {
-    inquirer
-        .prompt(employeeQuestions)
-        .then((answers) => {
-            console.log(answers);
-            const manager = new Manager(answers.name, answers.id, answers.email, answers.officeNumber);
-            console.log(manager);
-            return manager;
-        })
-    // }
+function runProgram() {
+    if (!employees) {
+        console.log('No employees yet. Enter name below to add first employee.');
+        addEmployee(managerPrompts)
+    // } else {
+    //     inquirer.prompt({
+    //         type: 'list',
+    //         message: 'Select role from list below:',
+    //         choices: ['engineer', 'intern',],
+    //         name: 'role'
+    //     },).then((answer) => {console.log(answer)})
+    //     .then(addEmployee(answer.role));
+    }
 };
 
-addManager();
+function addEmployee(q) {
+    inquirer.prompt([{
+        type: 'input',
+        message: `Enter name.`,
+        name: 'name',
+    }, {
+        type: 'number',
+        message: `Enter employee id.`,
+        name: 'id',
+    }, {
+        type: 'input',
+        message: `Enter email address.`,
+        name: 'email',
+    }])
+        .then((answers) => {
+            console.log(answers)
+            if (role === 'manager') {
+                console.log(`Adding a manager.`)
+                // addManager(answers);
+            } ;
+            // else if (role === 'engineer') {
+            //     addEngineer(answer);
+            // } else {
+            //     addIntern(answer);
+            // }
+        })
+        return answers;
+};
+
+function addManager() {
+    addEmployee('manager')
+    // .then(() => {
+    inquirer.prompt({
+            type: 'number',
+            message: `Enter manager's office number.`,
+            name: 'officeNum',
+    })
+    .then((answers) => {
+        const manager = new Manager(answers.name, answers.id, answers.email, answers.officeNum)
+        console.log(answer.officeNum)})
+    }
+    // )}
+
+runProgram();
--- prompts.js
+++ prompts.js
@@ -1,71 +1,47 @@
-const promptNewEmployee = {
-    promptRole:
-    {
-        type: 'list',
-        message: 'Select employee role from list below:',
-        choices: ['engineer', 'intern',],
-        name: 'role'
-    },
-    promptName:
-    {
-        type: 'input',
-        message: `Enter ${this.role}'s name.`,
-        name: 'name',
-    },
-    promptId:
-    {
-        type: 'number',
-        message: `Enter ${this.role}'s employee id.`,
-        name: 'id',
-    },
-    promptEmail:
-    {
-        type: 'input',
-        message: `Enter ${this.role}'s email address.`,
-        name: 'email',
-    },
-    promptOfficeNum:
-    {
-        type: 'number',
-        message: `Enter ${this.role}'s office number.`,
-        name: 'office',
-    },
-    promptGitHub:
-    {
-        type: 'input',
-        message: `Enter this ${this.role}'s GitHub user name.`,
-        name: 'github',
-        description: 'engineer'
-    },
-    promptSchool:
-    {
-        type: 'input',
-        message: `Enter the ${this.role} of this intern's school.`,
-        name: 'school',
-        description: 'intern'
-    },
-    promptAddMore:
-    {
-        type: 'confirm',
-        message: 'Would you like to add another employee?',
-        name: 'add'
+const allPrompts = {
+        promptRole: {
+            type: 'list',
+            message: 'Select role from list below:',
+            choices: ['engineer', 'intern',],
+            name: 'role'
+        },
+        promptName: {
+            type: 'input',
+            message: `Enter name.`,
+            name: 'name',
+        },
+        promptId: {
+            type: 'number',
+            message: `Enter employee id.`,
+            name: 'id',
+        },
+        promptEmail: {
+            type: 'input',
+            message: `Enter email address.`,
+            name: 'email',
+        },
+        promptOfficeNum: {
+            type: 'number',
+            message: `Enter manager's office number.`,
+            name: 'officeNum',
+        },
+        promptGitHub: {
+            type: 'input',
+            message: `Enter ${name}'s GitHub user name.`,
+            name: 'github',
+            description: 'engineer'
+        },
+        promptSchool: {
+            type: 'input',
+            message: `Enter the name of ${name}'s school.`,
+            name: 'school',
+            description: 'intern'
+        },
+        promptAddMore: {
+            type: 'confirm',
+            message: 'Would you like to add another employee?',
+            name: 'add'
+        }
     }
-};
 
-
-
-
-
-const managerPrompts = [
-    promptName, promptId, promptEmail, promptOfficeNum
-]
-console.log(managerPrompts)
-
-class Prompts {
-    constructor(role) {
-        this.role = role;
-        console.log(this.role);
-    }
-}
-
-module.exports = prompts;
+    module.export = prompts;
