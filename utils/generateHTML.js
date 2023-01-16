96dd460943edbbcf792159b7dd6530a567c93f08
Reed Keyes <rbkeyes@gmail.com>
2023-01-15 18:03:52 -0800

    adding styling

---
+++ utils/generateHTML.js
@@ -0,0 +1,4 @@
+const managerIcon = '<i class="fa-solid fa-business-time"></i>';
+const engineerIcon = '<i class="fa-solid fa-laptop-code"></i>';
+const internIcon = '<i class="fa-solid fa-graduation-cap"></i>';
+
