b1e1467444b941121b778effbbe1e5ec33568911
Reed Keyes <rbkeyes@gmail.com>
2023-01-16 21:06:50 -0800

    wrote draft html for generateHTML to write with employee info added.

--- utils/generateHTML.js
+++ utils/generateHTML.js
@@ -2,3 +2,6 @@ const managerIcon = '<i class="fa-solid fa-business-time"></i>';
 const engineerIcon = '<i class="fa-solid fa-laptop-code"></i>';
 const internIcon = '<i class="fa-solid fa-graduation-cap"></i>';
 
+function generateHTML(team) {
+
+};
