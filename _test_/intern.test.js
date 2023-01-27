b9670c7877dbaf018a1c15d8f7ac86984f1f4028
Reed Keyes <rbkeyes@gmail.com>
2023-01-26 17:30:22 -0800

    intern.test.js updated, all tests pass

--- _test_/intern.test.js
+++ _test_/intern.test.js
@@ -2,21 +2,41 @@
 const Intern = require('../lib/Intern');
 
 // const to be used for test
-const internObj = {name: 'Toni', id: 3, email: "toni@email.com", school: 'Pretty Good University'};
 const testObj = {name: 'Toni', id: 3, email: "toni@email.com", school: 'Pretty Good University'};
-const internTest = new Intern(testObj);
 
 // tests for intern 
 describe('Intern class', () => {
-    it('Should return an employee object containing name of school in addition to basic employee info', () => {
-        expect(internTest).toEqual(internObj);
+    const testIntern = new Intern(testObj);
+
+    it('Should return an instance of Intern class', () => {
+        expect(testIntern).toBeInstanceOf(Intern);
+    });
+    it('Should return typeof object', () => {
+        expect(typeof testIntern).toBe('object');
+    });
+    it('Should contain the Intern data entered by the user', () => {
+        expect(testIntern).toMatchObject(testObj);
+    });
+
+    describe('getSchool() method', () => {
+        const testSchool = new Intern(testObj).getSchool();
+
+        it('Should return typeof number', () => {
+            expect(typeof testSchool).toBe('string');
+        });
+        it('Should return input School value.', () => {
+            expect(testSchool).toBe('Pretty Good University');
+        });
     });
 
     describe('getRole method', () => {
+        const testRole = new Intern(testObj).getRole();
+
+        it('Should return typeof string', () => {
+            expect(typeof testRole).toBe('string');
+        });
         it('Should return "Intern"', () => {
-            const internRole = "Intern";
-            const roleTest = internTest.getRole();
-            expect(roleTest).toEqual(internRole);
-        })
+            expect(testRole).toEqual('Intern');
+        });
     });
 });
