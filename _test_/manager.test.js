124aa59c1b6c9c4cde38922557855679ef4ecb54
Reed Keyes <rbkeyes@gmail.com>
2023-01-26 17:17:25 -0800

    managerTest.js updated, all tests pass

--- _test_/manager.test.js
+++ _test_/manager.test.js
@@ -2,22 +2,43 @@
 const Manager = require('../lib/Manager');
 
 // const to be used for test
-const managerObj = {name: 'Jamie', id: 1, email: "jamie@email.com", officeNumber: 12};
-const testObj = {name: 'Jamie', id: 1, email: "jamie@email.com", officeNumber: 12};
-const managerTest = new Manager(testObj);
+const testObj = { name: 'Jamie', id: 1, email: "jamie@email.com", officeNumber: 12 };
+
 
 
 // tests for Manager 
 describe('Manager class', () => {
-    it('Should return an employee object containing office number in addition to basic employee info', () => {
-        expect(managerTest).toEqual(managerObj);
+    const testManager = new Manager(testObj);
+
+    it('Should return an instance of Manager class', () => {
+        expect(testManager).toBeInstanceOf(Manager);
+    });
+    it('Should return typeof object', () => {
+        expect(typeof testManager).toBe('object');
+    });
+    it('Should contain the manager data entered by the user', () => {
+        expect(testManager).toMatchObject(testObj);
+    });
+
+    describe('getOfficeNum() method', () => {
+        const testOfficeNum = new Manager(testObj).getOfficeNum();
+
+        it('Should return typeof number', () => {
+            expect(typeof testOfficeNum).toBe('number');
+        });
+        it('Should return input office number value.', () => {
+            expect(testOfficeNum).toBe(12);
+        });
     });
 
     describe('getRole method', () => {
+        const testRole = new Manager(testObj).getRole();
+
+        it('Should return typeof string', () => {
+            expect(typeof testRole).toBe('string');
+        });
         it('Should return "Manager"', () => {
-            const managerRole = "Manager";
-            const roleTest = managerTest.getRole();
-            expect(roleTest).toEqual(managerRole);
-        })
+            expect(testRole).toEqual('Manager');
+        });
     });
 });
